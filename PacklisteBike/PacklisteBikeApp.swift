0ff8e0a7af9b281e580381781d402a1b1cec4bb2
fabiocplr <153329903+fabiocplr@users.noreply.github.com>
2025-03-12 20:35:14 +0100

    Initial Commit

---
+++ PacklisteBike/PacklisteBikeApp.swift
@@ -0,0 +1,17 @@
+//
+//  PacklisteBikeApp.swift
+//  PacklisteBike
+//
+//  Created by Fabio Cappellaro on 12.03.25.
+//
+
+import SwiftUI
+
+@main
+struct PacklisteBikeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PacklisteBikeTests/PacklisteBikeTests.swift
@@ -0,0 +1,17 @@
+//
+//  PacklisteBikeTests.swift
+//  PacklisteBikeTests
+//
+//  Created by Fabio Cappellaro on 12.03.25.
+//
+
+import Testing
+@testable import PacklisteBike
+
+struct PacklisteBikeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
