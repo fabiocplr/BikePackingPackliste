c9071a79659dff2fda7988e2975b74065449e1b6
Fabio Cappellaro <153329903+fabiocplr@users.noreply.github.com>
2025-10-01 15:38:03 +0200

    Update PacklistViewModel.swift

--- PacklisteBike/PacklistViewModel.swift
+++ PacklisteBike/PacklistViewModel.swift
@@ -14,4 +14,33 @@ struct PackItem: Identifiable, Codable {
     }
 }
  
+// ViewModel für eine einzelne Packliste
+class PackListViewModel: ObservableObject, Identifiable {
+    let id = UUID() // Eindeutige ID für jede Packliste
+    @Published var name: String
+    @Published var items: [PackItem]
+
+    init(name: String, items: [PackItem] = []) {
+        self.name = name
+        self.items = items
+    }
+    var groupedItems: [String: [PackItem]] {
+            Dictionary(grouping: items, by: { $0.category })
+        }
+    
+    func addItem(name: String, category: String, quantity: Int, weightPerItem: Int?) {
+        let newItem = PackItem(name: name, category: category, quantity: quantity, weightPerItem: weightPerItem, packed: false)
+        items.append(newItem)
+    }
+
+    func togglePacked(id: UUID) {
+        if let index = items.firstIndex(where: { $0.id == id }) {
+            items[index].packed.toggle()
+        }
+    }
+    var totalWeight: Int {
+        items.reduce(0) { $0 + $1.totalWeight }
+    }
+
+}
 
